54d14cdd9fe191f3a3acdc12735078ab34c7cd27
wiF0n <44643222+wiF0n@users.noreply.github.com>
2019-04-01 22:45:13 +0200

    a

--- integration.R
+++ integration.R
@@ -8,7 +8,10 @@ library(cmna)
 
 # Get mass attuneation coefficient (as a function of energy(included)) from file
 data = read_delim("mu_Fe.txt", delim = " ")
-x = 0.299
+a_params = read_tsv("A_parameters.txt") %>% 
+  mutate(energy_prev = lag(energy, default = 0)) 
+
+x = 0.598
 rho = 7.879
 data <- data %>% mutate(mu = rho*mass_att_coef, 
                         mu_prev = lag(mu, default = Inf),
@@ -23,7 +26,7 @@ ggplot(data, aes(x = energy, y = mu)) +
   scale_y_log10()
 
 # Interpolate function
-linear_params <- data.frame(pwiselinterp(data$energy, data$mu))
+linear_params <- tbl_df(pwiselinterp(data$energy, data$mu))
 linear_params <- rbind(data.frame(m = 0, b = 0), linear_params)
 
 data_and_params <- cbind(data, linear_params)
@@ -45,4 +48,26 @@ energy_mean_calc <- data_and_params %>%
   mutate(mean_energy = (mu_mean - b)/m)
 
 energy_mean <- energy_mean_calc$mean_energy
-                                    
+
+a_linear_params_A1 <- tbl_df(pwiselinterp(a_params$energy, a_params$A1)) 
+a_linear_params_Alfa1 <- tbl_df(pwiselinterp(a_params$energy, a_params$Alfa1))
+a_linear_params_Alfa2 <- tbl_df(pwiselinterp(a_params$energy, a_params$Alfa2))
+
+a_linear_params_A1 <- rbind(data.frame(m = 0, b = 0), a_linear_params_A1)%>% 
+  rename(m_A1 = m,
+         b_A1 = b)
+a_linear_params_Alfa1 <- rbind(data.frame(m = 0, b = 0), a_linear_params_Alfa1)%>% 
+  rename(m_Alfa1 = m,
+         b_Alfa1 = b)
+a_linear_params_Alfa2 <- rbind(data.frame(m = 0, b = 0), a_linear_params_Alfa2)%>% 
+  rename(m_Alfa2 = m,
+         b_Alfa2 = b)
+
+a_linear_params <- cbind(a_params, a_linear_params_A1, a_linear_params_Alfa1, a_linear_params_Alfa2) 
+
+a_params_calc <- a_linear_params %>% 
+  mutate(in_interval = if_else(energy_mean >= energy_prev & energy_mean < energy, 1, 0)) %>% 
+  filter(in_interval == 1) %>% 
+  mutate(A1_calc = m_A1*energy_mean + b_A1,
+         Alfa1_calc = m_Alfa1*energy_mean + b_Alfa1,
+         Alfa2_calc = m_Alfa2*energy_mean + b_Alfa2)
